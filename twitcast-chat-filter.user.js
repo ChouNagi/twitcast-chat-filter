247dcb72b1364b423bdb1e9f490ac114385fd0bd
ChouNagi <84361618+ChouNagi@users.noreply.github.com>
2022-09-18 11:34:49 +0100

    initial commit
    skeleton to ensure the links are correct

---
+++ twitcast-chat-filter.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         Twitcast Chat Filter
+// @namespace    https://github.com/ChouNagi
+// @version      0.0.1
+// @exclude      *
+// @grant        none
+// ==/UserScript==
